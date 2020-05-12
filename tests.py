577ac3c5dd2df8bc5c5b80b49b440716d4083e13
Sébastien Lange <lange.sebastien@gmail.com>
2020-05-12 18:51:34 +0200

    fix tests

--- tests.py
+++ tests.py
@@ -1,18 +1,14 @@
-import importlib.util
-import numpy as np
+import unittest
+
+import schedule
+from mock import patch
+
+import camera_capture
 
 try:
     from picamera import PiCamera
 except ImportError:
-    from fake_rpi.picamera import PiCamera
-
-import unittest
-import camera_capture
-import logging
-import schedule
-
-from unittest import mock
-from unittest.mock import MagicMock
+    from fake_picamera import PiCamera
 
 
 class TestCameraCaptureMethods(unittest.TestCase):
@@ -33,20 +29,19 @@ class TestCameraCaptureMethods(unittest.TestCase):
         config = None
         config = camera_capture.schedule_jobs(camera_capture.read_config(config), config)
 
-        self.assertTrue(any(['take pictures' in job.tags for job in schedule.jobs]))
+        self.assertTrue(any(['take {count} pictures' in job.tags for job in schedule.jobs]))
 
         take_picture_jobs = [job for job in schedule.jobs if 'take pictures' in job.tags]
         expected_job_count = sum([len(job['at']) for job in config['scheduled_jobs'] if job['tag'] == 'take pictures'])
         self.assertEqual(len(take_picture_jobs), expected_job_count)
 
-    def test_take_pictures_does_camera_capture(self):
-        camera = PiCamera()
-        camera.capture = MagicMock()
+    @patch.object(PiCamera, 'capture')
+    @patch('time.sleep', return_value=None)
+    def test_take_pictures_calls_camera_capture(self, patched_time_sleep, capture):
+        camera_capture.config = camera_capture.read_config(None)
+        camera_capture.take_pictures()
 
-        config = camera_capture.read_config(None)
-        camera_capture.take_pictures(config)
-
-        camera.capture.assert_called()
+        capture.assert_called()
 
 
 if __name__ == '__main__':
