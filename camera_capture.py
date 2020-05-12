67a1e5c24c830463102f53ff9b9899e278c7b2cb
Sébastien Lange <lange.sebastien@gmail.com>
2020-05-12 21:22:52 +0200

    refactor config file

--- camera_capture.py
+++ camera_capture.py
@@ -111,7 +111,7 @@ def initialize(new_config, old_config):
     if old_config is None:
         logging.info(f'Starting with configuration: {new_config}')
         switch_light('off' if new_config['light_only_for_pictures'] else 'on')
-    elif new_config['preview']['light_only_for_pictures'] != old_config['light_only_for_pictures']:
+    elif new_config['light_only_for_pictures'] != old_config['light_only_for_pictures']:
         switch_light('off' if new_config['light_only_for_pictures'] else 'on')
 
 
